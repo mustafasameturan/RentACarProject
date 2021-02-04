10d0ecef4d894bf0a9a169bbb2ff3ebe54fe85d4
mustafasameturan <mustafasameturan@hotmail.com>
2021-02-04 12:40:04 +0300

    04.02.2021 Updated

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using System;
 
 namespace ConsoleUI
 {
@@ -6,7 +8,13 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Id + " - " + car.Description);
+            }
+
         }
     }
 }
